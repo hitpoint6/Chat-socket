1675fccaca26c32c0093a1fe79ae9ce6b45836cd
hitpoint6 <hitpoint6@gmail.com>
2020-11-18 10:20:32 -0800

    update asgi

--- chat2/asgi.py
+++ chat2/asgi.py
@@ -1,19 +1,28 @@
-# mysite/asgi.py
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
-import chat.routing
 
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat2.settings")
 django_asgi_app = get_asgi_application()
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from chat.consumers import AdminChatConsumer, PublicChatConsumer
+
 application = ProtocolTypeRouter({
-  "http": django_asgi_app,
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^chat/admin/$", AdminChatConsumer.as_asgi()),
+            url(r"^chat/$", PublicChatConsumer.as_asgi()),
+        ])
     ),
 })
